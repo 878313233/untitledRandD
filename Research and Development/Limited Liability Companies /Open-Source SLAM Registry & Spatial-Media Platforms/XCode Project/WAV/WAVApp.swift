71540a1c63d9037dc71da0f13108d28900dd80eb
Luis Angel <lsoberanispacheco@bearcubs.santarosa.edu>
2025-12-18 02:50:31 -0800

    Add files via upload

---
+++ Research and Development/Limited Liability Companies /Open-Source SLAM Registry & Spatial-Media Platforms/XCode Project/WAV/WAVApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAVApp.swift
+//  WAV
+//
+//  Created by Luis Angel on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WAVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
